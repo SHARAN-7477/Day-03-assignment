5a4e307f9c22f981f4e07fd5766d3fc4fde51e00
SHARAN M <121308881+SHARAN-7477@users.noreply.github.com>
2022-12-25 21:32:38 +0530

    create my own resume in json format

---
+++ script.js
@@ -0,0 +1,41 @@
+let myResume={
+      "basics": {
+        "name": "SHARAN M",
+        "email": "sharan7477@gamil.com",
+        "phone": 9500554031,
+        "degree": "PGDM- Logistics and supply chain managment ",
+        "location": {
+          "address": "1G/129 Rajgopal nagar 2nd street ",
+          "postalCode": 628008,
+          "city": "Tuticorin",
+          "state": "Tamilnadu",
+          "country": "India"
+        },
+        "profiles": [
+          {
+            "website": "https://www.linkedin.com/in/sharan-11mm0307/",
+          }
+        ]
+      },
+      "education": [
+        {
+          "institution": "Loyola college",
+          "department": "PGDM",
+          "studyType": "fulltime",
+          "batch start year": 2021,
+          "batch end year": 2022,
+          "gpa": 7,
+        }
+      ]
+      "language": [
+        {
+          "languages": "Tamil,Enlish",
+        }
+      ],
+      "interests": [
+        {
+          "name": "Reading books,",
+        }
+      ]
+    }
+    console.log(myResume);
